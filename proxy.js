567ac1d3c7ed2cf84a6d4e246563af869e0bf323
Ken <nethriondev@gmail.com>
2026-05-21 04:12:27 +0000

    update

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 export const config = { runtime: 'edge' };
 const WORKER_URL = 'https://proxy-embed.nethriondev.workers.dev';
-
+//
 export default async function handler(request) {
   const url = new URL(request.url);
   const workerUrl = new URL(url.pathname + url.search, WORKER_URL);
